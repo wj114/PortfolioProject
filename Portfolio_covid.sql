36cd5b9c6ba9cae8f78c27b08b0c0fa66da25841
wj114 <64145811+wj114@users.noreply.github.com>
2024-11-03 22:58:59 +0800

    Add files via upload

--- Portfolio_covid.sql
+++ Portfolio_covid.sql
@@ -22,5 +22,55 @@ SELECT location, max(total_cases) as HighestInfection_Count
 FROM Portfolio_Project.coviddeaths
 where continent is not null
 group by location 
-order by HighestInfection_Count desc
+order by HighestInfection_Count desc;
 
+
+#Global numbers
+select date, sum(new_cases) as total_cases, sum(new_deaths) as total_death, round(sum(new_deaths)*100/sum(new_cases),3) as total_death_percentage
+FROM Portfolio_Project.coviddeaths
+where continent is not null
+group by date;
+
+
+#Total number of case, deaths and percentage
+select sum(new_cases) as total_cases, sum(new_deaths) as total_death, round(sum(new_deaths)*100/sum(new_cases),3) as total_death_percentage
+FROM Portfolio_Project.coviddeaths
+where continent is not null;
+#group by date
+
+
+#Looking at total polulation vs vaccinations
+
+select d.continent, d.location, d.date, v.population, v.new_vaccinations,
+	sum(new_vaccinations) over (partition by d.location order by d.location, d.date) as Cumulative_people_vaccinated
+from Portfolio_Project.coviddeaths d
+join  Portfolio_Project.covidvaccination v
+	on d.location= v.location and d.date=v.date
+where d.continent is not null;
+
+# use CTE
+
+with cte as 
+(select d.continent, d.location, d.date, v.population, v.new_vaccinations,
+	sum(new_vaccinations) over (partition by d.location order by d.location, d.date) as Cumulative_people_vaccinated
+from Portfolio_Project.coviddeaths d
+join  Portfolio_Project.covidvaccination v
+	on d.location= v.location and d.date=v.date
+where d.continent is not null)
+
+select *,
+	Cumulative_people_vaccinated *100/population as percentatge_vaccination
+from cte;
+
+#create view
+create view percentatge_vaccination as
+select d.continent, d.location, d.date, v.population, v.new_vaccinations,
+	sum(new_vaccinations) over (partition by d.location order by d.location, d.date) as Cumulative_people_vaccinated
+from Portfolio_Project.coviddeaths d
+join  Portfolio_Project.covidvaccination v
+	on d.location= v.location and d.date=v.date
+where d.continent is not null;
+
+
+select *
+from percentatge_vaccination
